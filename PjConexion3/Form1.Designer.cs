0f8a35aa6055d5460ec1a6f5b39f528560280281
Edwin Daniel Baltazar Terbullino <edwin.baltazar@tecsup.edu.pe>
2020-03-02 19:23:49 -0500

    fix

---
+++ PjConexion3/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace PjConexion3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.DgProductos = new System.Windows.Forms.DataGridView();
+            this.LblCantidad = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.DgProductos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(231, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(272, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Lista de Productos - Mediante App.config ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(231, 331);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(146, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Número de Productos";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // DgProductos
+            // 
+            this.DgProductos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DgProductos.Location = new System.Drawing.Point(117, 119);
+            this.DgProductos.Name = "DgProductos";
+            this.DgProductos.RowTemplate.Height = 24;
+            this.DgProductos.Size = new System.Drawing.Size(506, 165);
+            this.DgProductos.TabIndex = 2;
+            this.DgProductos.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DgProductos_CellContentClick);
+            // 
+            // LblCantidad
+            // 
+            this.LblCantidad.Location = new System.Drawing.Point(433, 326);
+            this.LblCantidad.Name = "LblCantidad";
+            this.LblCantidad.Size = new System.Drawing.Size(100, 22);
+            this.LblCantidad.TabIndex = 3;
+            this.LblCantidad.TextChanged += new System.EventHandler(this.LblCantidad_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.LblCantidad);
+            this.Controls.Add(this.DgProductos);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DgProductos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView DgProductos;
+        private System.Windows.Forms.TextBox LblCantidad;
+    }
+}
+
---
+++ PjConexion3/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+
+namespace PjConexion3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["Baltazar"].ConnectionString);
+
+        public void ListaProductos()
+        {
+            using (SqlDataAdapter Df = new SqlDataAdapter("Usp_ListaProductos_Neptuno", cn))
+            {
+                Df.SelectCommand.CommandType = CommandType.StoredProcedure;
+                using (DataSet Da = new DataSet())
+                {
+                    Df.Fill(Da, "Productos");
+                    DgProductos.DataSource = Da.Tables["Productos"];
+                    LblCantidad.Text = Da.Tables["Productos"].Rows.Count.ToString();
+                }
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LblCantidad_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DgProductos_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ListaProductos();
+        }
+    }
+}
